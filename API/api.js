9ff038edc29a3e2e4a1748b757fcb4541c15108c
LennartEdberg <lennartedb@hotmail.se>
2015-03-27 05:03:58 +0100

    API&Lightsout design

--- API/api.js
+++ API/api.js
@@ -1,8 +1,6 @@
-
 $("#PictureBtn").click(function(){
-;
-    var userName = $("#username").val();
-    userName = userName.toLowerCase();
+    var userName = $("#username").val().toLowerCase();
+    console.log(userName);
     var photoHTML = "<ul class='flex-container'>"
 $.getJSON("https://api.instagram.com/v1/users/search?q=" + userName + "&access_token=1726136215.a0b9446.733d63f25cd04b6fa0d0f953bf73b299&callback=?", function(instagram){
     if(instagram.meta.code == 200 && instagram.data.length > 0)
@@ -12,8 +10,7 @@ $.getJSON("https://api.instagram.com/v1/users/search?q=" + userName + "&access_t
     {
 
     $.getJSON("https://api.instagram.com/v1/users/" + data.id + "/media/recent/?access_token=1726136215.a0b9446.733d63f25cd04b6fa0d0f953bf73b299&count=50&callback=?", function(userFeedObj){
-        console.log(userFeedObj);
-        if(userFeedObj.meta.code === 200)
+        if(userFeedObj.meta.code == 200)
         {
             
             $.each(userFeedObj.data, function(i, userFeed)
@@ -26,9 +23,9 @@ $.getJSON("https://api.instagram.com/v1/users/search?q=" + userName + "&access_t
             $("#PictureBtn").addClass("button active")
             photoHTML += "</ul>"
             $("#photoList").html(photoHTML);
-            $("h1").html(data.username);
+            $("#user").html(data.username);
             }
-        else if(userFeedObj.meta.code === 400)
+        else
         {
             alert("Användarens profil är privat!");
         }
@@ -45,22 +42,23 @@ else
 
 
 $("#StatBtn").click(function(){
-    var userName = $("#username").val();
-        userName = userName.toLowerCase();
+    var userName = $("#username").val().toLowerCase();
     var photoHTML = "<div class='center-text'>"
     var likescount = 0;
     var followers = 0;
     var likesbild = 0;
     $.getJSON("https://api.instagram.com/v1/users/search?q=" + userName + "&access_token=1726136215.a0b9446.733d63f25cd04b6fa0d0f953bf73b299&callback=?", function(instagram){
+        console.log(instagram);
     if(instagram.meta.code == 200 && instagram.data.length > 0)
     {
+        
     $.each(instagram.data, function(i, data){
     if(userName === data.username)
     {
         
     $.getJSON("https://api.instagram.com/v1/users/" + data.id + "/media/recent/?access_token=1726136215.a0b9446.733d63f25cd04b6fa0d0f953bf73b299&count=33&callback=?", function(userFeedObj){
 
-        if(userFeedObj.meta.code === 200)
+        if(userFeedObj.meta.code == 200)
         {
             $.each(userFeedObj.data, function(i, userFeed)
             {
@@ -84,10 +82,10 @@ $.getJSON("https://api.instagram.com/v1/users/" + data.id + "/?access_token=1726
             photoHTML += "<h2>Namn: <span class='orangetxt'>" + data.full_name + "</span></h2><br> <img class='instaprofil' src=" + data.profile_picture + "><br> <h2>Antal bilder: <span class='orangetxt'>" + userFeedObj.data.length + "</span></h2> <br> <h2>Totala likes: <span class='orangetxt'>" + likescount +  "</span></h2> <br> <h2>Ungefärligt antal likes per bild: <span class='orangetxt'>" + likesbild + "</span></h2> <br> <h2>Antal följare: <span class='orangetxt'>" + followers + "</span></h2>";
             photoHTML +="</div>"
             $("#photoList").html(photoHTML);
-            $("h1").html(data.username);
+            $("#user").html(data.username);
     })
 }
-        if(userFeedObj.meta.code === 400)
+        else
         {
             alert("Användarens profil är privat!");
         }
@@ -102,3 +100,4 @@ $.getJSON("https://api.instagram.com/v1/users/" + data.id + "/?access_token=1726
         }
 })
 })
+//ACCESS-TOKEN: 1726136215.a0b9446.733d63f25cd04b6fa0d0f953bf73b299
